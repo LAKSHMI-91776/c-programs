53e0d35adfbb9300ffd6feeeae5b7bfaed8e65d6
LAKSHMI-91776 <chillamanisha4@gmail.com>
2026-04-22 16:12:42 +0530

    Add files via upload

---
+++ symbol.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter the characters\n");
+    scanf("%c",&ch);
+    int res=!((ch>='A')&&(ch<='Z')) && !((ch>='a')&&(ch<='z')) && !((ch>='0')&&(ch<='9')); 
+    printf("%c is a symbol i.e is:%d",ch,res);
+
+    return 0;
+}
